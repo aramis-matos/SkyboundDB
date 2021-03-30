63b90a72de99dfe95a053544382d2a3a239e592d
username <email>
2021-03-30 13:47:05 -0400

    Fixed small formatting bug. Call function for text printing on button still persists

--- SkyBoundDB_GUI.py
+++ SkyBoundDB_GUI.py
@@ -42,7 +42,7 @@ class Character_select:
         Djeetabutton.pack(side = LEFT, anchor = N)
 #how will it print different buttons for each move?
     def example_selection_mapping(self):
-        move_ACXXXbutton = Button(self.frame2, text = self.test_move_printing, command = self.change_state2)
+        move_ACXXXbutton = Button(self.frame2, text = self.test_move_printing(), command = self.change_state2)
         move_ACXXXbutton.pack(pady = 10)
 
     def change_state(self, state):
@@ -64,11 +64,11 @@ class Character_select:
 
     def test_move_printing(self):
         chr_name = 'gran'
-        make_touple = ["Move: ", "Damage: ", "Guard: ", "Startup: ", "Active: ", "Recovery: ", "On Block: ", "On Hit: "]
+        make_touple = ["Damage: ", "Guard: ", "Startup: ", "Active: ", "Recovery: ", "On Block: ", "On Hit: "]
 
         chr_selected = Character('0', str(chr_name))
         chr_selectedMove = list(chr_selected.df[chr_selected.moves[4]])
-        string_format = chr_selected.moves[4] + "\n"
+        string_format = "Move: " + chr_selected.moves[4] + "\n"
         for i in range(len(chr_selectedMove)):
             string_format = string_format + str(make_touple[i]) + str(chr_selectedMove[i]) + "\n"
         print(string_format)
