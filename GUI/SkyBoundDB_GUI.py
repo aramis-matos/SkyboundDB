445c99211cbb3f585b2690cdc371746cb5cf0a99
Aramis Matos <aramis.matos1@gmail.com>
2021-03-31 18:21:37 -0400

    Made preparations for comparison

--- GUI/SkyBoundDB_GUI.py
+++ GUI/SkyBoundDB_GUI.py
@@ -8,11 +8,12 @@ import os
 
 class Character_select:
     def __init__(self):
+        self.selected_moves = []
         self.root = tk.Tk()
         self.root.title("SkyBoundDB")
 
         self.frame = tk.Frame(self.root)
-        self.frame2 = tk.Frame(self.root)
+        self.frame2 = tk.Frame(self.root, bg='black')
 
         # Default packed frame
         self.frame.pack()
@@ -43,10 +44,18 @@ class Character_select:
 
     def button_mapping(self):
         faceButtons = []
-        for i in range(len(self.skyfarer)):
-            faceButtons.append(tk.Button(self.frame, image=self.skyfarer[i],
-                                         command=lambda: [self.change_state(), self.show_moves(i)]))
-            faceButtons[i].pack(side=tk.LEFT, anchor=tk.N)
+        faceButtons.append(
+            tk.Button(self.frame, image=self.skyfarer[0],
+                      command=lambda: [self.change_state(), self.show_moves(0)]))
+        faceButtons.append(
+            tk.Button(self.frame, image=self.skyfarer[1],
+                      command=lambda: [self.change_state(), self.show_moves(1)]))
+        faceButtons.append(
+            tk.Button(self.frame, image=self.skyfarer[2],
+                      command=lambda: [self.change_state(), self.show_moves(2)]))
+        faceButtons[0].pack(side=tk.LEFT, anchor=tk.N)
+        faceButtons[1].pack(side=tk.LEFT, anchor=tk.N)
+        faceButtons[2].pack(side=tk.LEFT, anchor=tk.N)
 
     def show_moves(self, char_id):
         self.buttons_text = []
@@ -58,16 +67,23 @@ class Character_select:
         moveButtons = []
         for k in range(len(moves)):
             self.buttons_text.append(tk.StringVar())
-            moveButtons.append(tk.Button(self.frame2, textvariable=self.buttons_text[k],
-                                         command=[self.print_moves_to_button(temp_character, moves[k], k)]))
+            moveButtons.append(tk.Label(self.frame2, textvariable=self.buttons_text[k], fg='white', bg='gray'))
+                                         #command=[self.print_moves_to_button(temp_character, moves[k], k)]))
+            self.buttons_text[k] = self.print_moves_to_button(temp_character, moves[k], k)
 
             columnNum += 1
             if columnNum >= 3:
                 columnNum = 0
                 rowNum += 1
             moveButtons[k].grid(row=rowNum, column=columnNum)
-        cancelButton = tk.Button(self.frame2, text="Cancel", command=lambda: [self.change_state2()], fg='red')
-        cancelButton.grid(row=rowNum + 1, column=1, pady=5)
+        entryLabel = tk.Label(self.frame2, text="Put Move Name Here")
+        self.getNameOfMove = tk.Entry(self.frame2)
+        entryLabel.grid(row=rowNum+1, column=1)
+        cancelButton = tk.Button(self.frame2, text="Cancel", command=lambda: [self.setEntryValue(), self.select_move(char_id, self.tempVal), self.change_state2()], fg='red')
+        cancelButton.grid(row=rowNum+1, column=0, pady=5)
+        self.getNameOfMove.grid(row=rowNum+1, column=2)
+
+
 
     def change_state(self):
         print("From Character_Select to Move_Select")
@@ -76,13 +92,23 @@ class Character_select:
 
     def change_state2(self):
         print("From Move_Select to Character_Select")
-        self.frame2.pack_forget()
+        self.frame2.destroy()
+        self.frame2 = tk.Frame(self.root, bg='black')
         self.frame.pack()
 
     def print_moves_to_button(self, character, move, buttons_text_id):
         moveToPrint = character.df[move]
         string = character.returnMoveStr(moveToPrint, move)
         self.buttons_text[buttons_text_id].set(string)
+
+    def select_move(self, char_id, move):
+        if len(self.selected_moves) >= 2:
+            self.selected_moves.pop(0)
+        self.selected_moves.append((characterRoster[char_id], move))
+        print(self.selected_moves)
+
+    def setEntryValue(self):
+        self.tempVal = self.getNameOfMove.get()
 
 
 p = Character_select()
