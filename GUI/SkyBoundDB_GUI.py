8e1275493bfbcd4a74c345f3ded874fe9ff55f27
Aramis Matos <aramis.matos1@gmail.com>
2021-04-01 15:41:51 -0400

    fixed compareButton and created compareFrame

--- GUI/SkyBoundDB_GUI.py
+++ GUI/SkyBoundDB_GUI.py
@@ -7,14 +7,14 @@ import os
 class Character_select:
     def __init__(self):
         self.selected_moves = []
+        self.selectedNum = 0
         self.root = tk.Tk()
         self.root.title("SkyBoundDB")
-
-        self.frame = tk.Frame(self.root)
-        self.frame2 = tk.Frame(self.root, bg='black')
+        self.character_frame = tk.Frame(self.root)
+        self.move_frame = tk.Frame(self.root, bg='black')
 
         # Default packed frame
-        self.frame.pack()
+        self.character_frame.pack()
 
         self.skyfarer = Character_select.character_images(self)
         Character_select.button_mapping(self)
@@ -32,7 +32,6 @@ class Character_select:
         for characters in range(len(characterRoster)):
             rosterList.append(baseDir + characterRoster[characters] + imageSuffix)
         New_Character_photo = []
-
         for i in range(0, len(rosterList)):
             Character_photo = Image.open(rosterList[i])
             Character_resized = Character_photo.resize((138, 138), Image.ANTIALIAS)
@@ -44,13 +43,18 @@ class Character_select:
         faceButtons = []
         for i in range(len(characterRoster)):
             faceButtons.append(
-                tk.Button(self.frame, image=self.skyfarer[i],
-                          command=lambda z=i: [self.change_state(), self.show_moves(z)]))
+                tk.Button(self.character_frame, image=self.skyfarer[i],
+                          command=lambda z=i: [self.to_move_select(), self.incrementSelectedNum(), self.show_moves(z),
+                                               self.update_compare_button()]))
+            faceButtons[i].bind("<Button-3>", lambda event, k=i: [self.select_move(k, ""), self.incrementSelectedNum(),
+                                                                  self.update_compare_button()])
             faceButtons[i].pack(side=tk.LEFT, anchor=tk.N)
+        self.compareButton = tk.Button(self.character_frame, text='Compare', state=tk.DISABLED,
+                                       command=lambda: [print(self.selected_moves), self.to_compare_screen()])
+        self.compareButton.pack()
 
     def show_moves(self, char_id):
         self.buttons_text = []
-
         temp_character = Character(0, characterRoster[char_id])
         self.moves = temp_character.moves
         rowNum = 0
@@ -58,29 +62,29 @@ class Character_select:
         self.moveButtons = []
         for k in range(len(self.moves)):
             self.buttons_text.append(tk.StringVar())
-            self.moveButtons.append(tk.Button(self.frame2, textvariable=self.buttons_text[k], fg='white', bg='gray',
+            self.moveButtons.append(tk.Button(self.move_frame, textvariable=self.buttons_text[k], fg='white', bg='gray',
                                               command=lambda j=k: [self.select_move(char_id, self.moves[j]),
-                                                                   self.change_state2()]))
+                                                                   self.to_character_select()]))
             self.print_moves_to_button(temp_character, self.moves[k], k)
             columnNum += 1
             if columnNum >= 3:
                 columnNum = 0
                 rowNum += 1
             self.moveButtons[k].grid(row=rowNum, column=columnNum)
-        cancelButton = tk.Button(self.frame2, text="Cancel",
-                                 command=lambda: [self.change_state2()], fg='red')
+        cancelButton = tk.Button(self.move_frame, text="Cancel",
+                                 command=lambda: [self.to_character_select()], fg='red')
         cancelButton.grid(row=rowNum + 1, column=1, pady=5)
 
-    def change_state(self):
+    def to_move_select(self):
         print("From Character_Select to Move_Select")
-        self.frame.pack_forget()
-        self.frame2.pack()
+        self.character_frame.pack_forget()
+        self.move_frame.pack()
 
-    def change_state2(self):
+    def to_character_select(self):
         print("From Move_Select to Character_Select")
-        self.frame2.destroy()
-        self.frame2 = tk.Frame(self.root, bg='black')
-        self.frame.pack()
+        self.move_frame.destroy()
+        self.move_frame = tk.Frame(self.root, bg='black')
+        self.character_frame.pack()
 
     def print_moves_to_button(self, character, move, buttons_text_id):
         moveToPrint = character.df[move]
@@ -91,10 +95,20 @@ class Character_select:
         if len(self.selected_moves) >= 2:
             self.selected_moves.pop(0)
         self.selected_moves.append((characterRoster[char_id], move))
-        print(self.selected_moves)
 
-    def setEntryValue(self):
-        self.tempVal = self.getNameOfMove.get()
+    def incrementSelectedNum(self):
+        self.selectedNum += 1
+        print(self.selectedNum)
+
+    def update_compare_button(self):
+        if self.selectedNum >= 2:
+            self.compareButton['state'] = tk.NORMAL
+
+    def to_compare_screen(self):
+        self.character_frame.pack_forget()
+        self.compare_screen_frame = tk.Frame(self.root, bg='red')
+        self.compare_screen_frame.pack()
+
 
 
 p = Character_select()
