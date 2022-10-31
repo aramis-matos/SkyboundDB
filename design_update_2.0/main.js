587d19fe5dd627ae6089f3301b47015bd86539d8
aramis-matos <aramis.matos1@gmail.com>
2022-10-31 07:29:30 -0400

    added changing portraits

--- design_update_2.0/main.js
+++ design_update_2.0/main.js
@@ -1,4 +1,4 @@
-// function getFrameData() {
+function getFrameData() {
     let cats = document.querySelector("div.attack-info thead").innerText;
     cats = cats.replace("\n", "").split("\t");
     const stats = [].slice.call(document.querySelector("div.attack-info"));
@@ -21,64 +21,57 @@
     frameData = frameData.map(fd => Object.fromEntries(fd.map((x, i) => [cats[i], x])))
     moveSet = Object.fromEntries(fullMoves.map((x, i) => [x, frameData[i]]))
     console.log(Object.keys(moveSet));
-    // document.querySelector("body").style.display = "none";
-    // document.querySelector("body").append(`${Object.entries(moveSet)}`)
-//     return moveset;
-// }
+    document.querySelector("body").style.display = "none";
+    document.querySelector("body").append(`${Object.entries(moveSet)}`)
+    return moveset;
+}
 
-// [
-//     "Anre",
-//     "Avatar Belial",
-//     "Beelzebub",
-//     "Belial",
-//     "Cagliostro",
-//     "Charlotta",
-//     "Djeeta",
-//     "Eustace",
-//     "Ferry",
-//     "Gran",
-//     "Katalina",
-//     "Ladiva",
-//     "Lancelot",
-//     "Lowain",
-//     "Metera",
-//     "Narmaya",
-//     "Percival",
-//     "Seox",
-//     "Soriz",
-//     "Vaseraga",
-//     "Vira",
-//     "Yuel",
-//     "Zeta",
-//     "Zooey"
-//   ]
+const characters = [
+    "Anre",
+    "Avatar Belial",
+    "Beelzebub",
+    "Belial",
+    "Cagliostro",
+    "Charlotta",
+    "Djeeta",
+    "Eustace",
+    "Ferry",
+    "Gran",
+    "Katalina",
+    "Ladiva",
+    "Lancelot",
+    "Lowain",
+    "Metera",
+    "Narmaya",
+    "Percival",
+    "Seox",
+    "Soriz",
+    "Vaseraga",
+    "Vira",
+    "Yuel",
+    "Zeta",
+    "Zooey"
+]
 
-//   [
-//     "Anre",
-//     "Avatar Belial",
-//     "Beelzebub",
-//     "Belial",
-//     "Cagliostro",
-//     "Charlotta",
-//     "Djeeta",
-//     "Eustace",
-//     "Ferry",
-//     "Gran",
-//     "Katalina",
-//     "Ladiva",
-//     "Lancelot",
-//     "Lowain",
-//     "Metera",
-//     "Narmaya",
-//     "Percival",
-//     "Seox",
-//     "Soriz",
-//     "Vaseraga",
-//     "Vira",
-//     "Yuel",
-//     "Zeta",
-//     "Zooey"
-//   ]
+// // x = [].slice.call(document.querySelectorAll("div.div-col dl a[title^=GBVS] > b")) #GET ALL CHARACTERS FROM https://dustloop.com/w/Granblue_Fantasy_Versus
+// // x.map(z => z.innerText).sort((x,y) => x >= y)
 
-// x = [].slice.call(document.querySelectorAll("div.div-col dl a[title^=GBVS] > b")) #GET ALL CHARACTERS FROM https://dustloop.com/w/Granblue_Fantasy_Versus
-// x.map(z => z.innerText).sort((x,y) => x >= y)
+let char_sels = document.querySelectorAll("form");
+for (let sel of char_sels) {
+    let form = sel.elements.characters
+    for (let char of characters) {
+        let option = document.createElement("option");
+        option.value = char
+        option.innerText = char
+        form.appendChild(option)
+    }
+}
+char_sels[0].elements.characters.value = "Gran"
+char_sels[1].elements.characters.value = "Djeeta"
+
+char_sels.forEach(sel => sel.addEventListener("change", e => {
+    let img = sel.querySelector("img")
+    let character = sel.elements.characters.value;
+    img.src = `sprites/${character}_portrait.png`
+    img.alt = `${character} Sprite`
+}))
